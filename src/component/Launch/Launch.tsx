0f2788323a30b0164f41bed96a03bbcdba2bc992
daniyal214 <daniyal.hashmi2010@yahoo.com>
2020-09-06 02:56:53 +0500

    'sixth-commit'

--- src/component/Launch/Launch.tsx
+++ src/component/Launch/Launch.tsx
@@ -9,34 +9,49 @@ import 'aos/dist/aos.css';
 
 
 interface Props {
-    data: LaunchesQuery
+    data: LaunchesQuery,
+    frameborder: string
+    details: string
+    is_tentative: boolean
+    launch_site: {
+        site_name: string
+    }
+    launch_success: boolean
+    launch_year: number
+    links: {
+        flickr_images: string[]
+        video_link: string[]
+    }
+    mission_id: string[]
+    mission_name: string
+    upcoming: string
 }
 
 const Launch: React.FC<Props> = ({ data }) => {
     useEffect(() => {
         Aos.init({ duration: 2000 });
-      }, []);
+    }, []);
 
     console.log(data)
     if (!data.launches) {
         return <div>Launch unavailable</div>
     }
     return (
-        <Grid  container direction='row' spacing={3} alignItems='center' justify='center' >
+        <Grid container direction='row' spacing={3} alignItems='center' justify='center' >
             {data.launches.map(launch =>
                 <Grid data-aos="fade-up" container item component={Card} xs={12} sm={8} md={3} wrap='wrap'
                     style={{ margin: '2%', maxWidth: '100%', backgroundColor: '#c5c6c7' }} direction='row' >
                     <CardMedia style={{ height: 0, paddingTop: '1%', maxHeight: '100%', }} />
                     <img src={launch?.links?.flickr_images[0]} alt='launch' style=
-                            {{ maxWidth: '100%', maxHeight: '30vh', minHeight: '50vh', minWidth: '100%' }} />
-                    <CardContent style={{color: '#1f2833'}} >
-                        <Typography variant='h6'  gutterBottom align='justify'> <b>Mission Name: </b> <br />  {launch?.mission_name} </Typography>
-                        <Typography variant='subtitle1'  align='justify'><b>Year: </b> {launch?.launch_year}</Typography>
+                        {{ maxWidth: '100%', maxHeight: '30vh', minHeight: '50vh', minWidth: '100%' }} />
+                    <CardContent style={{ color: '#1f2833' }} >
+                        <Typography variant='h6' gutterBottom align='justify'> <b>Mission Name: </b> <br />  {launch?.mission_name} </Typography>
+                        <Typography variant='subtitle1' align='justify'><b>Year: </b> {launch?.launch_year}</Typography>
                         <Typography variant='subtitle1' align='justify' gutterBottom><b>Launch Site Name: </b> {launch?.launch_site?.site_name}</Typography>
                         <Link to={`/launch/${launch?.mission_name}`} >
-                            <Button 
+                            <Button
                                 style={{ marginLeft: 0, backgroundColor: '#1f2833', color: '#c5c6c7' }}
-                                size='medium' variant="contained"> See More  
+                                size='medium' variant="contained"> See More
                             </Button>
                         </Link>
                     </CardContent>
--- src/component/Rockets/Rockets.tsx
+++ src/component/Rockets/Rockets.tsx
@@ -8,7 +8,34 @@ import 'aos/dist/aos.css';
 
 
 interface Props {
-    data: RocketsQuery
+    data: RocketsQuery,
+    active: boolean
+        boosters: number
+        company: string
+        cost_per_launch: number
+        country: string
+        description: string
+        engines: {
+            number: number
+            type: string
+            version: string
+            __typename: string
+        }
+        first_flight: string
+        diameter: {
+            meters: string
+        }
+        flickr_images: string[]
+        mass: {
+            kg: number
+            __typename: string
+        }
+        rocket_id: string
+        rocket_name: string
+        rocket_type: string
+        stages: number
+        success_rate_pct: number
+        __typename: string
 }
 
 const Rockets: React.FC<Props> = ({ data }) => {
